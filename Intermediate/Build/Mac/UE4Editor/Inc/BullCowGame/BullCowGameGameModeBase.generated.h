7c7ae48fd87b70bbf3033da38511055c30ec5227
jomeke <10506008+curiousjazz77@users.noreply.github.com>
2020-10-09 16:52:24 -0500

    first commit

---
+++ Intermediate/Build/Mac/UE4Editor/Inc/BullCowGame/BullCowGameGameModeBase.generated.h
@@ -0,0 +1,95 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef BULLCOWGAME_BullCowGameGameModeBase_generated_h
+#error "BullCowGameGameModeBase.generated.h already included, missing '#pragma once' in BullCowGameGameModeBase.h"
+#endif
+#define BULLCOWGAME_BullCowGameGameModeBase_generated_h
+
+#define TripleX_4_25_Source_BullCowGame_BullCowGameGameModeBase_h_15_SPARSE_DATA
+#define TripleX_4_25_Source_BullCowGame_BullCowGameGameModeBase_h_15_RPC_WRAPPERS
+#define TripleX_4_25_Source_BullCowGame_BullCowGameGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS
+#define TripleX_4_25_Source_BullCowGame_BullCowGameGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesABullCowGameGameModeBase(); \
+	friend struct Z_Construct_UClass_ABullCowGameGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(ABullCowGameGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/BullCowGame"), NO_API) \
+	DECLARE_SERIALIZER(ABullCowGameGameModeBase)
+
+
+#define TripleX_4_25_Source_BullCowGame_BullCowGameGameModeBase_h_15_INCLASS \
+private: \
+	static void StaticRegisterNativesABullCowGameGameModeBase(); \
+	friend struct Z_Construct_UClass_ABullCowGameGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(ABullCowGameGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/BullCowGame"), NO_API) \
+	DECLARE_SERIALIZER(ABullCowGameGameModeBase)
+
+
+#define TripleX_4_25_Source_BullCowGame_BullCowGameGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API ABullCowGameGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ABullCowGameGameModeBase) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ABullCowGameGameModeBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ABullCowGameGameModeBase); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ABullCowGameGameModeBase(ABullCowGameGameModeBase&&); \
+	NO_API ABullCowGameGameModeBase(const ABullCowGameGameModeBase&); \
+public:
+
+
+#define TripleX_4_25_Source_BullCowGame_BullCowGameGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API ABullCowGameGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ABullCowGameGameModeBase(ABullCowGameGameModeBase&&); \
+	NO_API ABullCowGameGameModeBase(const ABullCowGameGameModeBase&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ABullCowGameGameModeBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ABullCowGameGameModeBase); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ABullCowGameGameModeBase)
+
+
+#define TripleX_4_25_Source_BullCowGame_BullCowGameGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET
+#define TripleX_4_25_Source_BullCowGame_BullCowGameGameModeBase_h_12_PROLOG
+#define TripleX_4_25_Source_BullCowGame_BullCowGameGameModeBase_h_15_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	TripleX_4_25_Source_BullCowGame_BullCowGameGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	TripleX_4_25_Source_BullCowGame_BullCowGameGameModeBase_h_15_SPARSE_DATA \
+	TripleX_4_25_Source_BullCowGame_BullCowGameGameModeBase_h_15_RPC_WRAPPERS \
+	TripleX_4_25_Source_BullCowGame_BullCowGameGameModeBase_h_15_INCLASS \
+	TripleX_4_25_Source_BullCowGame_BullCowGameGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define TripleX_4_25_Source_BullCowGame_BullCowGameGameModeBase_h_15_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	TripleX_4_25_Source_BullCowGame_BullCowGameGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	TripleX_4_25_Source_BullCowGame_BullCowGameGameModeBase_h_15_SPARSE_DATA \
+	TripleX_4_25_Source_BullCowGame_BullCowGameGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+	TripleX_4_25_Source_BullCowGame_BullCowGameGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+	TripleX_4_25_Source_BullCowGame_BullCowGameGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> BULLCOWGAME_API UClass* StaticClass<class ABullCowGameGameModeBase>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID TripleX_4_25_Source_BullCowGame_BullCowGameGameModeBase_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS

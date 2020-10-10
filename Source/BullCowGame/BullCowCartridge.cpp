a837b31a76eb5324ac3d0d62def1c7692282c0b2
jomeke <10506008+curiousjazz77@users.noreply.github.com>
2020-10-09 22:33:00 -0500

    Abstract guess handling to function

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -19,24 +19,8 @@ void UBullCowCartridge::OnInput(const FString& Input) // When the player hits en
     if (bGameOver){
         ClearScreen();
         InitGame();
-    } else {
-
-    
-        if (Input == HiddenWord){
-                PrintLine(TEXT("You have Won!"));
-                EndGame();
-        } else {
-            --Lives;
-            PrintLine(TEXT("Lost a life!"));
-            if (Lives > 0){
-                if (Input.Len() != HiddenWord.Len()){
-                    PrintLine(TEXT("Sorry, try guess again! \n You have %i lives remaining"), Lives);
-                }
-        } else{
-            PrintLine(TEXT("You have no lives left"));
-    }
-        }
-
+    } else { //Checking Payer Guess
+        ProcessGuess(Input);
 }
 }
 
@@ -46,7 +30,7 @@ void UBullCowCartridge::InitGame() {
     PrintLine(TEXT("Welcome to Bull Cows!!"));
     HiddenWord = TEXT("cake");
     // Set Lives
-    Lives = 4;
+    Lives = 5;
     bGameOver = false;
     
     PrintLine(TEXT("Guess the %i letter word!"), HiddenWord.Len());
@@ -57,4 +41,22 @@ void UBullCowCartridge::InitGame() {
 void UBullCowCartridge::EndGame() {
     bGameOver = true;
     PrintLine(TEXT("Press enter to play again"));
+}
+
+void UBullCowCartridge::ProcessGuess(FString Guess) {
+        if (Guess == HiddenWord){
+                PrintLine(TEXT("You have Won!"));
+                EndGame();
+        } else {
+            --Lives;
+            PrintLine(TEXT("Lost a life!"));
+            if (Lives > 0){
+                if (Guess.Len() != HiddenWord.Len()){
+                    PrintLine(TEXT("Sorry, try guess again! \n You have %i lives remaining"), Lives);
+                }
+        } else{
+            PrintLine(TEXT("You have no lives left"));
+    }
+        }
+
 }
